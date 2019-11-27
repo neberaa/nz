5c27aa9ebb8b683c0f47811a7b71871c0a5244cf
Nebera Anton <anebera@apextech.com.ua>
2019-11-27 06:42:19 -0700

    - client only rendering added to full page component

--- src/main.js
+++ src/main.js
@@ -2,15 +2,17 @@ import DefaultLayout from "~/layouts/Default.vue";
 import ResponsiveImage from "~/components/ResponsiveImage.vue";
 import store from './store';
 import deviceChecker from './deviceChecker';
-import VueFullPage from 'vue-fullpage.js'
 import 'fullpage.js/dist/fullpage.css';
 
-export default function(Vue, { head, appOptions }) {
+export default function(Vue, { head, appOptions, isClient  }) {
   Vue.component("Layout", DefaultLayout);
   Vue.component("ResponsiveImage", ResponsiveImage);
   head.htmlAttrs = { lang: 'ru' };
   Vue.mixin(deviceChecker);
-  Vue.use(VueFullPage);
+  if (isClient) {
+    const VueFullPage = require('vue-fullpage.js').default;
+    Vue.use(VueFullPage);
+  }
   
   appOptions.store = store;
 }
