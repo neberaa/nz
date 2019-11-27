532b58b5f4f790a4a19259f9c71e5e694a626775
Nebera Anton <anebera@apextech.com.ua>
2019-11-27 08:24:28 -0700

    - add mouse parallax package

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,7 +6,22 @@
           First section ...
         </div>
         <div class="section">
+          <parallax-container class="image-container">
           Second section ...
+            <parallax-element
+              tag="p"
+              type="translation"
+              :parallaxStrength="20">
+              gravity
+            </parallax-element>
+            <parallax-element
+              tag="p"
+              type="translation"
+              :parallaxStrength="-20">
+              Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Nulla porttitor accumsan tincidunt. Pellentesque in ipsum id orci porta dapibus. Vivamus suscipit tortor eget felis porttitor volutpat. Cras ultricies ligula sed magna dictum porta.
+            </parallax-element>
+            <parallax-element tag="p" type="translation" :parallaxStrength="5">LOREM</parallax-element>
+          </parallax-container>
         </div>
         <div class="section">
           Third section ...
@@ -48,6 +63,14 @@ export default {
       import ('vue-fullpage.js')
         .then(m => m.FullPage)
         .catch(),
+    ParallaxContainer: () =>
+      import ('vue-mouse-parallax')
+        .then(m => m.ParallaxContainer)
+        .catch(),
+    ParallaxElement: () =>
+      import ('vue-mouse-parallax')
+        .then(m => m.ParallaxElement)
+        .catch(),
   },
   metaInfo () {
     return {
@@ -85,6 +108,13 @@ export default {
   text-align: center;
   @include screenBreakpoint2(phone) {
     font-size: 2rem;
+  }
+  .image-container {
+    max-width: calc(100% - 60px);
+    margin: auto;
+    @include screenBreakpoint2(desktop) {
+      max-width: 800px;
+    }
   }
 }
 </style>
