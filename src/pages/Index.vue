e797f9f145a02fc36b92a8969369560a9bbfe057
Nebera Anton <anebera@apextech.com.ua>
2020-06-03 16:16:20 +0300

    - change parallax package - add some demo pictures

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,27 +2,31 @@
   <Layout>
     <ClientOnly>
       <full-page ref="fullpage" :options="options" id="fullpage">
-        <div class="section">
+        <div class="section ocean-1">
           First section ...
         </div>
-        <div class="section">
-          Second section....
-<!--          <parallax-container class="image-container">-->
-<!--          Second section ...-->
-<!--            <parallax-element-->
-<!--              tag="p"-->
-<!--              type="translation"-->
-<!--              :parallaxStrength="20">-->
-<!--              gravity-->
-<!--            </parallax-element>-->
-<!--            <parallax-element-->
-<!--              tag="p"-->
-<!--              type="translation"-->
-<!--              :parallaxStrength="-20">-->
-<!--              Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Nulla porttitor accumsan tincidunt. Pellentesque in ipsum id orci porta dapibus. Vivamus suscipit tortor eget felis porttitor volutpat. Cras ultricies ligula sed magna dictum porta.-->
-<!--            </parallax-element>-->
-<!--            <parallax-element tag="p" type="translation" :parallaxStrength="5">LOREM</parallax-element>-->
-<!--          </parallax-container>-->
+        <div class="section ocean">
+          <kinesis-container class="image-container">
+          Second section ...
+            <kinesis-element
+              tag="p"
+              type="translate"
+              :strength="20">
+              gravity
+            </kinesis-element>
+            <kinesis-element
+              tag="p"
+              type="translate"
+              :strength="-20">
+              Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Nulla porttitor accumsan tincidunt. Pellentesque in ipsum id orci porta dapibus. Vivamus suscipit tortor eget felis porttitor volutpat. Cras ultricies ligula sed magna dictum porta.
+            </kinesis-element>
+            <kinesis-element
+              tag="p"
+              type="translate"
+              :strength="5">
+              LOREM
+            </kinesis-element>
+          </kinesis-container>
         </div>
         <div class="section">
           Third section ...
@@ -49,14 +53,14 @@ export default {
       import ('vue-fullpage.js')
         .then(m => m.FullPage)
         .catch(),
-    // ParallaxContainer: () =>
-    //   import ('vue-mouse-parallax')
-    //     .then(m => m.ParallaxContainer)
-    //     .catch(),
-    // ParallaxElement: () =>
-    //   import ('vue-mouse-parallax')
-    //     .then(m => m.ParallaxElement)
-    //     .catch(),
+    KinesisContainer: () =>
+      import ('vue-kinesis')
+        .then(m => m.KinesisContainer)
+        .catch(),
+    KinesisElement: () =>
+      import ('vue-kinesis')
+        .then(m => m.KinesisElement)
+        .catch(),
   },
   metaInfo () {
     return {
@@ -92,12 +96,17 @@ export default {
   @include screenBreakpoint2(phone) {
     font-size: 2rem;
   }
+  &.ocean-1 {
+    background: url('https://res.cloudinary.com/nzh/image/upload/v1591114786/nzh/5c49c62721348_qwufzp.jpg') center no-repeat;
+    background-size: cover;
+  }
+  &.ocean {
+    background: url('https://res.cloudinary.com/nzh/image/upload/v1591114025/nzh/ocean_yotq5j.jpg') center no-repeat;
+    background-size: cover;
+  }
   .image-container {
     max-width: calc(100% - 60px);
     margin: auto;
-    @include screenBreakpoint2(desktop) {
-      max-width: 800px;
-    }
   }
 }
 </style>
